499b94e927fd4a3d85c831db6a3ffdc578eeed46
unknown <libaolin@techen.cn>
2020-11-17 19:19:46 +0800

    项目创建

---
+++ forumbackend/src/main/java/com/lbl/forumbackend/ForumbackendApplication.java
@@ -0,0 +1,13 @@
+package com.lbl.forumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumbackendApplication.class, args);
+    }
+
+}
